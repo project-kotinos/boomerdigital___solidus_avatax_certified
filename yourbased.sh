35f7fcef40ba7282c66d6571dcac45b1133b9d13
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-12 17:53:11 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -2,6 +2,7 @@
 set -ex
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
+export RAILS_ENV=test
 apt-get install -y libmysqlclient-dev postgresql libpq-dev
 bundle install --jobs=3 --retry=3
 bundle exec rake
