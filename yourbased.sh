a074cd0cbd9d424fba2a47201361549a78650994
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-16 13:49:28 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 set -ex
-apt-get update
+
 export DEBIAN_FRONTEND=noninteractive
+export CI=true
+export TRAVIS=true
+export CONTINUOUS_INTEGRATION=true
+export USER=travis
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 export RAILS_ENV=test
-apt-get install -y libmysqlclient-dev postgresql libpq-dev
+export RACK_ENV=test
+export MERB_ENV=test
+export JRUBY_OPTS="--server -Dcext.enabled=false -Xcompile.invokedynamic=false"
+
+apt-get install -y libmysqlclient-dev postgresql libpq-dev libfontconfig
+
+#install phantomjs
+rm -rf $PWD/travis_phantomjs; mkdir -p $PWD/travis_phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar -xvf $PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64.tar.bz2 -C $PWD/travis_phantomjs
+PATH=$PWD/travis_phantomjs/phantomjs-2.1.1-linux-x86_64/bin:$PATH
+phantomjs --version
+
 bundle install --jobs=3 --retry=3
 bundle exec rake
